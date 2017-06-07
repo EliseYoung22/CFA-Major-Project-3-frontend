c6c0246ada03ea28ae0a435aea2eed16ce86eea9
Elise Young <eliseyoung_4@hotmail.com>
2017-06-07 11:44:58 +1000

    add background image and styling

---
+++ components/BackgroundImage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {AppRegistry, Image, StyleSheet } from 'react-native';
+
+
+class BackgroundImage extends Component {
+
+    render() {
+        return (
+            <Image source={require('../marble-back.jpg')} style={styles.backgroundImage}>
+            {this.props.children}
+            </Image>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    }
+});
+ module.exports = BackgroundImage;
